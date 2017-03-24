b1255d5923fa18c1b386e42b0b9f07c66aabd65b
thanh <caothanhphung2237@gmail.com>
2017-03-24 15:01:00 +0700

    Update server.js
    112

--- server.js
+++ server.js
@@ -37,23 +37,25 @@ app.post('/webhook', function(req, res) {
         // If user send text
         if (message.message.text) {
           var text = message.message.text;
+          sendMessage(senderId,text);
           var n = text.search("neu ai noi:");
           var m = text.search("thi e tra loi la:");
           var hoi = "",dap = "";
           if(n){
+            sendMessage(senderId,"123");
             hoi = text.substring(n+12,m);
           }
           if(m){
             dap = text.substrring(m+18,text.length);
             MongoClient.connect(url, function(err, db) {
               assert.equal(null, err);
-              console.log("Connected successfully to server");
                 insertDocuments(senderId,function(){
                   db.close();
                 });
            });
           }
           //--------------- function insert document to database
+          sendMessage(senderId,"insert");
           var insertDocuments = function(db, callback) {
             // Get the documents collection
             var collection = db.collection('user');
@@ -69,6 +71,7 @@ app.post('/webhook', function(req, res) {
             });
           }
           //-------------------function query database
+          sendMessage(senderId,"query");
           var findDocuments = function(db, callback) {
             // Get the documents collection
             var collection = db.collection('user');
