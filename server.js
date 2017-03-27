60924a86f0306efe340c2dc04c4303b0dc1b57ba
thanh <caothanhphung2237@gmail.com>
2017-03-27 11:24:06 +0700

    Update server.js
    as

--- server.js
+++ server.js
@@ -43,19 +43,19 @@ app.post('/webhook', function(req, res) {
           var MongoClient = require('mongodb').MongoClient , assert = require('assert');
           // Connection URL
           var url = 'mongodb://thanh:123456@ds137760.mlab.com:37760/bikbot_database';
-          if(n > -1){
-            hoi = text.substring(n+12,m);
-            if(m > -1){
-              dap = text.substring(m+18,text.length);
-              // Use connect method to connect to the server
-              MongoClient.connect(url, function(err, db) {
-                assert.equal(null, err);
-                insertDocuments(db, function() {
-                  db.close();
-                });
-              });
-            }
-          }
+//           if(n > -1){
+//             hoi = text.substring(n+12,m);
+//             if(m > -1){
+//               dap = text.substring(m+18,text.length);
+//               // Use connect method to connect to the server
+//               MongoClient.connect(url, function(err, db) {
+//                 assert.equal(null, err);
+//                 insertDocuments(db, function() {
+//                   db.close();
+//                 });
+//               });
+//             }
+//           }
           MongoClient.connect(url, function(err, db) {
             assert.equal(null, err);
             findDocuments(db, function() {
